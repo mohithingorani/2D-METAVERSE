e7f36ede449e680f6864dd6981f3b88ba19ad01e
Mohit <mohithingorani2003@gmail.com>
2026-01-13 16:43:43 +0000

    new

--- ws-server/src/index.ts
+++ ws-server/src/index.ts
@@ -1,13 +1,15 @@
 import { WebSocketServer } from 'ws';
+import { User } from './UserManager';
 
 const wss = new WebSocketServer({ port: 8080 });
 
 wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
+  let user = new User(ws);
 
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
+  ws.on('error',console.error);
+
+  ws.on("close",()=>{
+    user.destroy();
   });
-
-  ws.send('something');
 });
