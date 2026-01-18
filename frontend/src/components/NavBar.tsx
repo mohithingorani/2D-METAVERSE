86d8142477c4c86bfa63a4854f759445a02973e7
Mohit <mohithingorani2003@gmail.com>
2026-01-18 16:35:59 +0000

    added sign out

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,9 +1,19 @@
 import { User } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 export function NavBar({ username }: { username: string }) {
+  const navigate = useNavigate();
+  function signOut() {
+    localStorage.removeItem("token");
+    navigate("/signup");
+  }
+
   return (
-    <div className="flex justify-end p-4 gap-3  w-full">
-      <div>{username}</div>
-      <User />
+    <div className="flex justify-end  p-4 gap-8 font-google  w-full">
+      <button className="hover:text-gray-400" onClick={signOut}>Sign Out</button>
+      <div className="flex gap-2 justify-center cursor-pointer">
+        <div>{username}</div>
+        <User />
+      </div>
     </div>
   );
 }
